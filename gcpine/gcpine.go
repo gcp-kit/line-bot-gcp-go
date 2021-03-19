dbad3cd581ab5f6032023750c4d861caed3e834c
54m <30588003+54m@users.noreply.github.com>
2021-03-20 05:03:28 +0900

    feat: add GC🍍

---
+++ gcpine/gcpine.go
@@ -0,0 +1,16 @@
+package gcpine
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+// GCPine - Toolkit of the LINE Bot to work for Google Cloud Platform.
+type GCPine struct {
+	ErrMessages []linebot.SendingMessage
+	Function    map[TracerName]func(ctx context.Context, pine *GCPine, event *linebot.Event) ([]linebot.SendingMessage, error)
+	LiffFunc    map[string]func(r *http.Request, w http.ResponseWriter)
+	*linebot.Client
+}
