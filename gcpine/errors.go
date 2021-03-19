6edd688a4604b581fe293c52235976a76ad4e9eb
54m <30588003+54m@users.noreply.github.com>
2021-03-20 05:12:16 +0900

    update: add `ErrNoSetFunction` / `ErrInvalidMessageType`

--- gcpine/errors.go
+++ gcpine/errors.go
@@ -2,5 +2,11 @@ package gcpine
 
 import "fmt"
 
-// ErrEmptyMessages - []linebot.SendingMessage is empty
-var ErrEmptyMessages = fmt.Errorf("no message to send")
+var (
+	// ErrEmptyMessages - []linebot.SendingMessage is empty
+	ErrEmptyMessages = fmt.Errorf("no message to send")
+	// ErrNoSetFunction - function to be executed individually is not set
+	ErrNoSetFunction = fmt.Errorf("no set function")
+	// ErrInvalidMessageType - invalid message type
+	ErrInvalidMessageType = fmt.Errorf("invalid message type")
+)
