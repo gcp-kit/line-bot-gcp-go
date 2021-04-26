9ac9af65b09ada97b971120769eac0c0d1301067
54m <30588003+54m@users.noreply.github.com>
2021-04-26 19:21:52 +0900

    upgrade: SDK version(v7)

--- gcpine/gcpine.go
+++ gcpine/gcpine.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
 type (
--- gcpine/parse.go
+++ gcpine/parse.go
@@ -3,7 +3,7 @@ package gcpine
 import (
 	"encoding/json"
 
-	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
 	"golang.org/x/xerrors"
 )
 
